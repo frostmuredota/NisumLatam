91ac33be8c73ac6fd4c6c7210d790e9d63df7b95
rduran <rduran@nisum.com>
2015-12-01 12:10:53 -0300

    Update PDF Files

---
+++ josecasal.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="images/favicon.ico" rel="shortcut icon">
+    <title>Nisum Technologies | Janel</title>
+    <link rel="stylesheet" type="text/css" href="css/base/bootstrap/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/features/main.css">
+    <script type="text/javascript" src="js/jquery-1.10.2.js"></script>
+    <script type="text/javascript" src="js/html5shiv.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/customScripts.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="http://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-68520735-1', 'auto');
+		ga('send', 'pageview');
+   </script>
+	<style type="text/css"></style>
+	<?php include_once "lang.es.php" ?>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+</head>
+<body>
+    <?php include "header.php" ?>
+
+    <div class="content-wrapper">
+            <div class="banner-wrapper">
+                <div class="breadcrumb"><?php echo $lang['elemIWeAre100.php4']; ?></div>
+            </div>
+            <div class="col-md-12 content">
+				<section>
+					<h1><?php echo $lang['elemIMeetUpJose.php1']; ?></h1>
+					<p><?php echo $lang['elemIMeetUpJose.php2']; ?></p>
+					<p><?php echo $lang['elemIMeetUpJose.php3']; ?></p>
+					<p><?php echo $lang['elemIMeetUpJose.php4']; ?></p>
+					<p><?php echo $lang['elemIMeetUpJose.php5']; ?></p>
+					<p><?php echo $lang['elemIMeetUpJose.php6']; ?></p>
+					<p><?php echo $lang['elemIMeetUpJose.php7']; ?></p>
+					<p><a href="https://www.youtube.com/watch?v=bBImPbCKBnM&feature=youtu.be" target="_blank"><?php echo $lang['elemIMeetUpJose.php8']; ?></a></p>
+				</section>
+				<img src="images/VideoJCG.png" alt="Jose">
+				<div>
+					Compartir en
+				</div>
+				<div style="float:left">
+								<script>
+									document.write('<a target="_blank" href="http://twitter.com/home?status=' + encodeURIComponent("Leyendo http://www.nisumlatam.com/josecasal.php en Nisum Latam") + '"><img src="images/twitterIcon.png" alt="Comparte esto en Twitter" /></a>');
+								</script>
+                                <a href="https://www.linkedin.com/shareArticle?mini=true&url=http%3A//nisumlatam.com/josecasal.php&title=&summary=&source="><img src="images/linkedInIcon.png" alt="Comparte esto en LinkedIn" /></a>
+				</div></br>
+			</div>
+    </div>    
+    <?php include "footer.php" ?>
+</body></html>
